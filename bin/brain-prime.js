ce5354263d52ad7a7982262c13fda9c589b6dba0
upshevchenko <upshevchenko@gmail.com>
2025-07-09 23:30:07 +0300

    brain-prime

---
+++ bin/brain-prime.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+import { runGame, getRandomInt } from '../src/index.js';
+
+const isPrime = (num) => {
+  if (num < 2) return false;
+  for (let i = 2, max = Math.sqrt(num); i <= max; i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+};
+
+const generateRound = () => {
+  const question = getRandomInt(1, 100);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+runGame(description, generateRound);
