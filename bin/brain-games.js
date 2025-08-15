5892b55f937d50702296f3ae32f730b7e1fb2105
upshevchenko <upshevchenko@gmail.com>
2025-08-15 21:10:13 +0300

    Fix line

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-console.log('Welcome to the Brain Games!');
 import welcomeUser from '../src/cli.js';
+
+console.log('Welcome to the Brain Games!');
 welcomeUser();
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,6 @@
 import readlineSync from 'readline-sync';
 
-function __welcomeUser() {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+export default function welcomeUser() {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 }
-export default __welcomeUser;
