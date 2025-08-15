c49afda3889095952eca69e8e53b832f45ce3866
upshevchenko <upshevchenko@gmail.com>
2025-08-15 21:54:31 +0300

    Fix 1

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,11 @@
 export const getRandomInt = (min, max) => {
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    throw new TypeError('Parameters must be numbers');
+  }
   const minInt = Math.ceil(min);
   const maxInt = Math.floor(max);
+  if (minInt > maxInt) {
+    throw new RangeError('Min value cannot be greater than max');
+  }
   return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
 };
